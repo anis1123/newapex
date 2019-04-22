3b5462cd807cf45d677e235e8e5f33416e23c81e
Janakcoder <janakraj.chapagain@apexcollege.edu.np>
2019-04-22 15:36:55 +0545

    Create global.blade.php

---
+++ resources/views/global.blade.php
@@ -0,0 +1,120 @@
+@extends('layout.app')
+
+@section('content')
+<div class="overlay"></div>
+<section id="home">
+    <div class="particle"></div>
+
+
+
+
+</section>
+
+<div class="topic text-center mt-4 ">
+    <h5 class="heading">GLOBAL CONNECTION</h5>
+</div>
+
+
+
+<div class="universities px-5 pb-5">
+    <div class="row p-0 m-0">
+        <div class="col-md-4">
+            <div class="image">
+                <img src="https://apexcollege.edu.np/apexmain/wp-content/uploads/2018/04/padova-italy.jpg" alt="">
+                <h5 class="text-center pt-3">University of Padova, Italy</h5>
+
+
+            </div>
+        </div>
+        <div class="col-md-4">
+                <div class="image">
+                        <img src="https://apexcollege.edu.np/apexmain/wp-content/uploads/2018/04/padova-italy.jpg" alt="">
+                        <h5 class="text-center pt-3">University of Padova, Italy</h5>
+
+
+                    </div>
+            </div>
+            <div class="col-md-4">
+                    <div class="image">
+                            <img src="https://apexcollege.edu.np/apexmain/wp-content/uploads/2018/04/padova-italy.jpg" alt="">
+                            <h5 class="text-center pt-3">University of Padova, Italy</h5>
+
+
+                        </div>
+                </div>
+    </div>
+
+
+</div>
+
+
+<div class="paragraph px-5">
+    <div class="col-md-12 text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, reiciendis quam debitis quos laborum exercitationem modi explicabo maiores iste saepe qui natus nisi nam cupiditate praesentium nulla cumque doloribus at?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque error reprehenderit nisi. Facilis cumque, quo libero corrupti maxime sint quos illo. Laborum amet minima fuga ea deleniti odio cumque. Similique.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente maiores aut natus corrupti aspernatur, repudiandae voluptatem accusamus illo pariatur quam eveniet quaerat excepturi totam aliquam ab reiciendis, rem, fugiat eaque.
+    </div>
+    <div class="col-md-12 text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, reiciendis quam debitis quos laborum exercitationem modi explicabo maiores iste saepe qui natus nisi nam cupiditate praesentium nulla cumque doloribus at?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque error reprehenderit nisi. Facilis cumque, quo libero corrupti maxime sint quos illo. Laborum amet minima fuga ea deleniti odio cumque. Similique.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente maiores aut natus corrupti aspernatur, repudiandae voluptatem accusamus illo pariatur quam eveniet quaerat excepturi totam aliquam ab reiciendis, rem, fugiat eaque.
+        </div>
+
+</div>
+
+
+<div class="global-card p-5 pt-5">
+    <div class="row p-0 m-0">
+            <div class="col-md-4 px-3">
+                    <div class="card">
+                            <img src="{{asset('/img/a.jpg')}}" alt="" height="230">
+
+                            <div class="title text-center pt-4">
+                                <h5>Title</h5>
+                            </div>
+                            <div class="card-body text-justify">
+                                <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt commodi nemo eveniet inventore repellat perferendis odio, officiis magnam, cum vero aliquid accusamus aliquam pariatur, molestias ea quibusdam laboriosam laudantium esse!
+                                    </p>
+                                <button class="btn btn-secondary"  style="box-shadow:none"><a href="#" class="text-white">See More</a> </button>
+                            </div>
+
+                    </div>
+                </div>
+
+        <div class="col-md-4 px-3">
+                <div class="card">
+                        <img src="{{asset('/img/a.jpg')}}" alt="" height="230">
+
+                        <div class="title text-center pt-4">
+                            <h5>Title</h5>
+                        </div>
+                        <div class="card-body text-justify">
+                            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt commodi nemo eveniet inventore repellat perferendis odio, officiis magnam, cum vero aliquid accusamus aliquam pariatur, molestias ea quibusdam laboriosam laudantium esse!
+                                </p>
+                            <button class="btn btn-secondary"  style="box-shadow:none"><a href="#" class="text-white">See More</a> </button>
+                        </div>
+
+                </div>
+            </div>
+
+
+            <div class="col-md-4 px-3">
+                    <div class="card">
+                            <img src="{{asset('/img/a.jpg')}}" alt="" height="230">
+
+                            <div class="title text-center pt-4">
+                                <h5>Title</h5>
+                            </div>
+                            <div class="card-body text-justify">
+                                <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt commodi nemo eveniet inventore repellat perferendis odio, officiis magnam, cum vero aliquid accusamus aliquam pariatur, molestias ea quibusdam laboriosam laudantium esse!
+                                    </p>
+                                <button class="btn btn-secondary"  style="box-shadow:none"><a href="#" class="text-white">See More</a> </button>
+                            </div>
+
+                    </div>
+                </div>
+    </div>
+</div>
+
+@endsection
+
